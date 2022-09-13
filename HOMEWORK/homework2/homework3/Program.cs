96440d29ffff8e214c0bc6b8dd08987264ef30e9
valya <user@mail.ru>
2022-09-13 14:04:58 +0300

    add homework3

---
+++ HOMEWORK/homework2/homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine ("Введите число :" );
+int a = Convert.ToInt32(Console.ReadLine()); 
+ 
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+           
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+ 
+ 
+ 
+            Console.ReadKey();
+        
