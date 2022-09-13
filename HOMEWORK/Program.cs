fb69afc053850e5e660a591b88c9d6ac1338480b
valya <user@mail.ru>
2022-09-13 12:22:42 +0300

    add HOMEWORK

---
+++ HOMEWORK/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+  Console.WriteLine("Первое число больше второго");
+
+}
+ else if (num1 < num2)
+ {
+   Console.WriteLine ("Первое число меньше второго");
+
+ }
+ Console.ReadKey();
+
