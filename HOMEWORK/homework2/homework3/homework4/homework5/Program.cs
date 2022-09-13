2bde2dfe6791955624c8096037808be134a83595
valya <user@mail.ru>
2022-09-13 16:05:14 +0300

    add homework5

---
+++ HOMEWORK/homework2/homework3/homework4/homework5/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(number);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
